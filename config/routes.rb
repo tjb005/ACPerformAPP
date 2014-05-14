13e830c38a8cc58aefd642b16fd2bc5699dda314
tjb005 <tim.biason@gmail.com>
2014-05-14 21:19:45 +1000

    140514 Implement Design

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,11 @@ resources :users do
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
+  match '/apt',     to: 'static_pages#home',     via: 'get'
+  match '/env',     to: 'static_pages#env',     via: 'get'
+  match '/cfg',     to: 'static_pages#cfg',     via: 'get'
+  match '/cdlmel',  to: 'static_pages#cdlmel',  via: 'get'
+  match '/res',     to: 'static_pages#res',     via: 'get'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
